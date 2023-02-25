b96e062cd399e019dc31e9193e58e72c6e43eba3
Emanuel Tavares <Morche17@proton.me>
2023-02-24 19:54:12 -0800

    Adición de una función para insertar al principio 10000 elementos aleatorios entre 1 y el 101

--- main.rb
+++ main.rb
@@ -2,15 +2,21 @@
 require_relative 'src/lista_ordenada'
 
 lista = ListaOrdenada.new
-lista.insertar_al_principio(3)
-lista.insertar_al_principio(6)
-lista.insertar_al_principio(1)
-lista.insertar_al_final(9)
-lista.insertar_al_final(8)
+
+for i in(0..10000)
+
+    lista.insertar_al_principio(rand(1..101))
+
+end
+
+lista.ordenar
+# puts "\nLista ordenada:"
+# puts "\n #{lista.imprimir}"
+
+# lista.insertar_al_principio(6)
+# lista.insertar_al_principio(1)
+# lista.insertar_al_final(9)
+# lista.insertar_al_final(8)
 #lista.insertar_entre_nodos(4, 3)
 #lista.eliminar(6)
 lista.imprimir
-
-lista.ordenar
-puts "\nLista ordenada:"
-puts "\n #{lista.imprimir}"
