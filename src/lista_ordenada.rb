28ea7eabcef019d6fafbaed327a9be4deb5b1ca2
Emanuel Tavares <Morche17@proton.me>
2023-02-24 19:57:26 -0800

    Adición de una barra de progreso para indicar el progreso de la ordenación de elementos

--- src/lista_ordenada.rb
+++ src/lista_ordenada.rb
@@ -1,4 +1,5 @@
 require_relative 'lista_enlazada'
+require 'ruby-progressbar'
 
 class ListaOrdenada < ListaEnlazada
     def ordenar
@@ -11,9 +12,12 @@ class ListaOrdenada < ListaEnlazada
     
         valores_ordenados = valores.sort
         nueva_lista = ListaEnlazada.new
+
+        progressbar = ProgressBar.create(title: "Ordenando elementos", total: valores_ordenados.length)
     
         valores_ordenados.each do |valor|
             nueva_lista.insertar_al_final(valor)
+            progressbar.increment
         end
         
         @cabeza = nueva_lista.cabeza
