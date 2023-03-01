970e4f5f61eef45a6b845adab45ce9b1b493cf82
Emanuel Tavares <Morche17@proton.me>
2023-03-01 08:28:07 -0800

    Adición del método de ordenación por selección con su respectiva barra de progreso para saber el tiempo que le resta a la función para terminar

--- src/lista_ordenada.rb
+++ src/lista_ordenada.rb
@@ -14,12 +14,47 @@ class ListaOrdenada < ListaEnlazada
         nueva_lista = ListaEnlazada.new
 
         progressbar = ProgressBar.create(title: "Ordenando elementos", total: valores_ordenados.length)
-    
+
         valores_ordenados.each do |valor|
             nueva_lista.insertar_al_final(valor)
             progressbar.increment
         end
         
         @cabeza = nueva_lista.cabeza
+    end
+
+    def seleccion
+
+      ahorita = @cabeza
+      pb = ProgressBar.create(title: "Ordenando elementos", total: tamaño)
+
+      while ahorita
+        min_nodo = ahorita
+        nodo_siguiente = ahorita.siguiente
+
+        while nodo_siguiente
+          if nodo_siguiente.valor < min_nodo.valor
+            min_nodo = nodo_siguiente
+          end
+          nodo_siguiente = nodo_siguiente.siguiente
+        end
+
+        temp = ahorita.valor
+        ahorita.valor = min_nodo.valor
+        min_nodo.valor = temp
+        ahorita = ahorita.siguiente
+        pb.increment
+      end
+      pb.finish
+    end
+
+    def tamaño
+      ahorita = @cabeza
+      tamaño = 0
+      while ahorita
+        tamaño += 1
+        ahorita = ahorita.siguiente
+      end
+      tamaño
     end
 end
